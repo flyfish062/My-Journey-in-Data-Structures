a73e2847e762028a2f926b2a886ea3abab4dc140
flyfish062 <flyfish@qq.com>
2026-03-07 13:36:47 +0800

    update

---
+++ DataStructures/HighOrderFunction.java
@@ -0,0 +1,9 @@
+public class HighOrderFunction {
+    public static int do_twice(IntUnaryFunction f,int x){
+        return f.apply(f.apply(x));
+    }
+    public static void main(String[] args){
+        IntUnaryFunction tenx = new TenX();
+        System.out.println(do_twice(tenx,2));
+    }
+}
